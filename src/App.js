3f6aa69d9cc9c83ad83c290ec4f237a5f4b7b3e4
YaChugunov <informed.looser@gmail.com>
2022-07-02 18:31:30 +0300

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+import useJsonFetch from './Hooks/useJsonFetch';
+
+function CallCustomHook({ url, opts }) {
+  const [data, isLoading, hasError] = useJsonFetch(url, opts);
+  return (
+    <div className="App">
+      <h1>{data && data.status}</h1>
+      <h1>{isLoading && 'Is Loading'}</h1>
+      <h1>{hasError && 'Has Error'}</h1>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <CallCustomHook url={process.env.REACT_APP_DATA_URL} />
+      <hr />
+      <CallCustomHook url={process.env.REACT_APP_ERROR_URL} />
+      <hr />
+      <CallCustomHook url={process.env.REACT_APP_LOADING_URL} />
+      <hr />
     </div>
   );
 }
